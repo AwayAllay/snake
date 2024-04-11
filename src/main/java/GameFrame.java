38592658c0a7de96742ed94c01d1cab80b7e926c
Lukaos187 <lukas.jensen@web.de>
2024-04-11 19:34:50 +0200

    Add first steps to launcher animation

--- src/main/java/GameFrame.java
+++ src/main/java/GameFrame.java
@@ -4,9 +4,17 @@ import java.awt.*;
 public class GameFrame extends JFrame {
 
     JFrame frame;
+    ImageIcon image;
 
     public GameFrame(){
         frame = new JFrame();
+        try {
+            image = new ImageIcon(getClass().getResource("Snake.png"));
+            frame.setIconImage(image.getImage());
+
+        }catch (NullPointerException e){
+            System.out.println("Image not found");
+        }
         frame.setTitle("Snake");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(Color.WHITE);
--- src/main/java/LaunchFrame.java
+++ src/main/java/LaunchFrame.java
@@ -1,17 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LaunchFrame implements ActionListener {
 
     private final JButton button;
     private final JFrame frame;
 
+    private Timer timer;
+
     private JLabel head;
+    private JLabel letter;
     private ImageIcon image;
+    private JPanel panel;
     public LaunchFrame(){
 
         frame = new JFrame("Launch game");
+        timer = new Timer();
+
+        letter = new JLabel("SNAKE");
+        letter.setBounds(145, 100, 250, 60);
+        letter.setVisible(true);
+        letter.setFont(new Font("Arial", Font.BOLD, 60));
 
         try {
             image = new ImageIcon(getClass().getResource("Snake.png"));
@@ -21,40 +34,86 @@ public class LaunchFrame implements ActionListener {
             System.out.println("Image not found");
         }
 
-        JPanel panel = new JPanel(null);
+        panel = new JPanel(null);
         button = new JButton("Play game!");
         button.setBounds(125, 500,250, 60);
         button.setFocusable(false);
         button.addActionListener(this);
         panel.add(button);
+        panel.add(letter);
 
-        head = new SnakeHead(250, 300, 10, 10);
+        head = new SnakeHead(125, -10, 10, 10);
         head.setVisible(true);
         frame.add(head);
 
 
         frame.setSize(500, 700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setAlwaysOnTop(true);
         frame.setResizable(false);
         frame.add(panel);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
 
-        playIntroAnimation(); //FIXME
+        playIntroAnimation();
 
     }
 
     private void playIntroAnimation() {
 
         //250/ 4 = 62,5
-        for (int i = 0; i < 10; i++) {
-            SnakeTail tailpiece = new SnakeTail(head.getX(), head.getY(), 10, 10);
-            frame.add(tailpiece);
-            head.setLocation(head.getX(), head.getY() + 10);
-        }
-        frame.revalidate();
-        frame.repaint();
+        drawText();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                SnakeTail tailpiece = new SnakeTail(head.getX(), head.getY(), 10, 10);
+                panel.add(tailpiece);
+                runDown();
+            }
+        }, 3500, 200);
+    }
 
+    private void drawText() { //FIXME
+        String word = "SNAKE";
+        int wordWidht = word.length() * 50;
+        int xOffset = (frame.getWidth() - wordWidht) / 2;
+
+        for (int i = 0; i < word.length(); i++) {
+            Timer timer1 = new Timer();
+            timer1.scheduleAtFixedRate(new TimerTask() {
+
+                int i = 0;
+                @Override
+                public void run() {
+                    if (i < word.length()) {
+                        i++;
+                        JLabel letter = new JLabel(String.valueOf(word.charAt(i)));
+                        letter.setFont(new Font("Arial", Font.BOLD, 24));
+                        letter.setBounds(xOffset + i * 50, 200, 50, 50); // Positionierung der Buchstaben
+                        frame.add(letter);
+                    }else {
+                        cancel();
+                    }
+                }
+            }, 3000, 200);
+            //JLabel letter = new JLabel(String.valueOf(word.charAt(i)));
+            //letter.setFont(new Font("Arial", Font.BOLD, 24));
+            //letter.setBounds(xOffset + i * 50, 200, 50, 50); // Positionierung der Buchstaben
+            //frame.add(letter);
+        }
+    }
+
+    private void runUp(){
+        head.setLocation(head.getX(), head.getY() - 10);
+    }
+    private void runLeft(){
+        head.setLocation(head.getX() - 10, head.getY());
+    }
+    private void runDown(){
+        head.setLocation(head.getX(), head.getY() + 10);
+    }
+    private void runRight(){
+        head.setLocation(head.getX() + 10, head.getY());
     }
 
     @Override
